4e62e265cfcb120c0637127b38c84f695d1dc173
Lucas Beronne <luckberonne@gmail.com>
2024-08-09 11:59:10 -0300

    Cambios de pagina inicial

--- src/navigation.js
+++ src/navigation.js
@@ -1,4 +1,4 @@
-import { getPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -71,33 +71,23 @@ export const footerData = {
     {
       title: 'Product',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Alimenticios', href: '#' },
+        { text: 'Agroquimicos', href: '#' }
       ],
     },
     {
-      title: 'Platform',
+      title: 'Servicios',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'Namur Desktop', href: '#' },
+        { text: 'Consultoría en aspectos regulatorios', href: '#' },
+        { text: 'Registro de agroquímicos', href: '#' },
+        { text: 'Representación legal y comercial', href: '#' }
       ],
     },
     {
-      title: 'Support',
+      title: 'Contacto',
       links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
+        { text: 'Quiero Información', href: '#' },
+        { text: 'RRHH', href: '#' }
       ],
     },
     {
@@ -121,11 +111,9 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/namur' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/luckberonne"> Lucas Beronne</a> y <a class="text-blue-600 underline dark:text-muted" href="https://github.com/Nicolas-Perez-Costa"> Nicolas Costa</a> · All rights reserved.
   `,
 };
