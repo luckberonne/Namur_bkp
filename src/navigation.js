295f265d321ccfaca5525f5a0772be1c0f5f0195
Nicolas-Perez-Costa <nico-10-94@hotmail.com>
2024-08-15 20:53:51 -0300

    Modificacion de estructura

--- src/navigation.js
+++ src/navigation.js
@@ -3,51 +3,16 @@ import { getPermalink } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'About us',
+      text: 'Sobre nosotros',
       href: getPermalink('/about'),
     },
     {
       text: 'Productos',
-      links: [
-        {
-          text: 'Alimenticios',
-          href: getPermalink('/productosAlimenticios'),
-        },
-        {
-          text: 'Agroquimicos',
-          href: getPermalink('/productosAgroquimicos'),
-        },
-      ],
-    },
-    {
-      text: 'Servicios',
-      links: [
-        {
-          text: 'Consultoría en aspectos regulatorios',
-          href: getPermalink('/servicioConsultoria'),
-        },
-        {
-          text: 'Registro de agroquímicos',
-          href: getPermalink('/servicioRegistro'),
-        },
-        {
-          text: 'Representación legal y comercial',
-          href: getPermalink('/servicioRepresentacion'),
-        },
-      ],
+      href: getPermalink('/productos')
     },
     {
       text: 'Contacto',
-      links: [
-        {
-          text: 'Quiero Información',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'RRHH',
-          href: getPermalink('/contact'),
-        },
-      ],
+      href: getPermalink('/contact')
     },
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/namur', target: '_blank' }],
@@ -63,14 +28,6 @@ export const footerData = {
       ],
     },
     {
-      title: 'Servicios',
-      links: [
-        { text: 'Consultoría en aspectos regulatorios', href: '#' },
-        { text: 'Registro de agroquímicos', href: '#' },
-        { text: 'Representación legal y comercial', href: '#' }
-      ],
-    },
-    {
       title: 'Contacto',
       links: [
         { text: 'Quiero Información', href: '#' },
@@ -81,14 +38,5 @@ export const footerData = {
   secondaryLinks: [
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/luckberonne"> Lucas Beronne</a> y <a class="text-blue-600 underline dark:text-muted" href="https://github.com/Nicolas-Perez-Costa"> Nicolas Costa</a> · All rights reserved.
-  `,
+  ]
 };
