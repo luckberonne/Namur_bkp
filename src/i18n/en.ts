2db5a580319224d61ed243a80b0f7b8bd96cddbf
Nicolas-Perez-Costa <nico-10-94@hotmail.com>
2024-12-16 20:31:10 -0300

    Cambios en los idiomas

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -4,43 +4,6 @@ export const en = {
     contact: 'Contact',
     products: 'Products',
     aboutUs: 'About Us',
-
-    homes: {
-      title: 'Homes',
-      saas: 'Saas',
-      startup: 'Startup',
-      app: 'App Download',
-      personal: 'Personal',
-    },
-    pages: {
-      title: 'Pages',
-      features: 'Features (Anchor Link)',
-      services: 'Services',
-      product: 'Pricing',
-      aboutUs: 'About Us',
-      contactUs: 'Contact',
-      terms: 'Terms',
-      privacyPolicy: 'Privacy Policy',
-    },
-    landing: {
-      title: 'Landing',
-      leadGeneration: 'Lead Generation',
-      sales: 'Long-form Sales',
-      clickThrough: 'Click-Through',
-      squeeze: 'Squeeze',
-      product: 'Product Details (or Services)',
-      comingSoon: 'Coming Soon or Pre-Launch',
-    },
-    blog: {
-      title: 'Blog',
-      blogList: 'Blog List',
-      article: 'Article',
-      articleMdx: 'Article (with MDX)',
-      category: 'Category Page',
-      tag: 'Tag Page',
-    },
-    widgets: 'Widgets',
-    download: 'Download',
   },
   footer: {
     product: {
@@ -63,21 +26,107 @@ export const en = {
       title: 'Namur — Agricultural Solutions',
     },
     hero: {
-      cta: 'Get template',
-      cta2: 'Learn more',
       imageAlt: 'NamurHero image',
       title: {
         part1: 'Sales and advice ',
         part2: 'of agricultural products',
       },
-      subtitle: 'is a company dedicated to the sale of agricultural products, both food and foodstuffs as biochemists. It offers seeds, fertilizers, pesticides and crop management solutions, focusing on the productivity and sustainability. In addition, it provides technical advice to optimize its clients results in the countryside.',
-      description:
-        'Suitable for Startups, Small Business, SaaS websites, Professional Portfolios, Marketing websites, Landing Pages & Blogs.',
+      subtitle: ', we believe in the power of nature. Since 2018, we have specialized in offering food products of the highest quality for the wholesale market, both in Paraguay and for export. Our products, carefully selected and processed, guarantee freshness and health in each delivery. Our commitment is to be the bridge between producers and companies, facilitating access to essential ingredients that feed not only the body, but also happiness and well-being.',
+    }
+  },
+  aboutUs: {
+    title: 'The natural thing that feeds your happiness',
+    subtitle: 'Founded in 2018, Namur was born with the aim of connecting people with food products that contribute to their well-being and health. From the beginning, we have focused on offering high-quality natural ingredients, supporting local producers and promoting the agricultural wealth of Paraguay. "The natural thing that feeds your happiness." or as we say "Pe heko ypyva omongaruva nde vy\' apavē" is more than a slogan, it is our philosophy.',
+    mision: {
+      title: 'Our mision',
+      itemA : 'Short-term objectives:',
+      descriptionA: 'Consolidate our presence in Paraguay, optimize logistics processes and expand our product portfolio to satisfy the demands of fractionators and manufacturers.',
+      itemB: 'Medium-term objectives:',
+      descriptionB: 'Diversify our products (chickpeas, beans, anise), establish strategic alliances with local producers and certify quality under international standards.',
+      itemC: 'Long-term objectives: ',
+      descriptionC: 'Expand into international markets, bringing Paraguayan products to the world and positioning Lamur as an ambassador of national quality.',
+      
     },
+    values: {
+      title: 'Our Values',
+      subtitle: 'At Namur, our values ​​are the basis of every action and decision. We believe that commitment, trust and tradition are essential to offer quality products that respect nature and people. Our passion for natural things drives us to guarantee food safety at every stage of the process.',
+      item1 : '',
+      subtitleI1: '',
+      item2: '',
+      subtitleI2: '',
+      item3: '',
+      subtitleI3: '',
+    },
+    achievements: {
+      title: '',
+      subtitle: '',
+      item1 : '',
+      subtitleI1: '',
+      item2: '',
+      subtitleI2: '',
+      item3: '',
+      subtitleI3: '',
+    }
   },
-  blog: {
-    title: 'The Blog',
-    subtitle:
-      'A statically generated blog example with news, tutorials, resources and other interesting content related to Namur',
+  products: {
+    title: '',
+    subtitle: '',
+    grillaProd: {
+      title: '',
+      subtitle: '',
+
+      oregano: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      lino: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      pasaDeUva: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      chia: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      sesamo: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      coriandro: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      albahaca: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      romero: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      proximamente: {
+        title: 'boooca',
+        description: 'boca',
+      },
+    }
   },
+  contact: {
+    title: '',
+    subtitle: '',
+    description: '',
+    form: {
+      name: '',
+      email: '',
+      message: '',
+      consent: '',
+      button: '',
+      message2: ''
+    },
+    visit: ''
+  },
+
+
 };
--- src/i18n/es.ts
+++ src/i18n/es.ts
@@ -4,43 +4,6 @@ export const es = {
     contact: 'Contacto',
     products: 'Productos',
     aboutUs: 'Sobre Nosotros',
-
-    homes: {
-      title: 'Inicio',
-      saas: 'SaaS',
-      startup: 'Startup',
-      app: 'Descarga de la App',
-      personal: 'Personal',
-    },
-    pages: {
-      title: 'Páginas',
-      features: 'Características (Enlace Ancla)',
-      services: 'Servicios',
-      product: 'Precios',
-      aboutUs: 'Sobre Nosotros',
-      contactUs: 'Contacto',
-      terms: 'Términos',
-      privacyPolicy: 'Política de Privacidad',
-    },
-    landing: {
-      title: 'Landing',
-      leadGeneration: 'Generación de Leads',
-      sales: 'Ventas de Formato Largo',
-      clickThrough: 'Clic a Través',
-      squeeze: 'Squeeze',
-      product: 'Detalles del Producto (o Servicios)',
-      comingSoon: 'Próximamente o Pre-Lanzamiento',
-    },
-    blog: {
-      title: 'Blog',
-      blogList: 'Lista de Blogs',
-      article: 'Artículo',
-      articleMdx: 'Artículo (con MDX)',
-      category: 'Página de Categoría',
-      tag: 'Página de Etiqueta',
-    },
-    widgets: 'Widgets',
-    download: 'Descargar',
   },
   footer: {
     product: {
@@ -63,21 +26,104 @@ export const es = {
       title: 'Namur — Soluciones Agrícolas',
     },
     hero: {
-      cta: 'Obtener plantilla',
-      cta2: 'Aprender más',
       imageAlt: 'Imagen de NamurHero',
-      title: {
+      title: {  
         part1: 'Ventas y asesoramiento ',
         part2: 'de productos agrícolas',
       },
-      subtitle: 'es una empresa dedicada a la venta de productos agrícolas, tanto alimentarios como biocomerciales. Ofrece semillas, fertilizantes, pesticidas y soluciones de manejo de cultivos, enfocándose en la productividad y sostenibilidad. Además, brinda asesoría técnica para optimizar los resultados de sus clientes en el campo.',
-      description:
-        'Adecuado para Startups, Pequeñas Empresas, sitios web SaaS, Portafolios Profesionales, sitios web de Marketing, Landing Pages y Blogs.',
-    },
+      subtitle: ', creemos en el poder de lo natural. Desde 2018, nos especializamos en ofrecer productos alimenticios de la más alta calidad para el mercado mayorista, tanto en Paraguay como para exportación. Nuestros productos, cuidadosamente seleccionados y procesados, garantizan frescura y sanidad en cada entrega. Nuestro compromiso es ser el puente entre productores y empresas, facilitando el acceso a ingredientes esenciales que alimentan no solo el cuerpo, sino también la felicidad y el bienestar.',
+    }
   },
-  blog: {
-    title: 'El Blog',
-    subtitle:
-      'Un ejemplo de blog generado estáticamente con noticias, tutoriales, recursos y otro contenido interesante relacionado con Namur',
+  aboutUs: {
+    title: 'Lo natural que alimenta tu felicidad',
+    subtitle: 'Fundada en 2018, Namur nació con el objetivo de conectar a las personas con productos alimenticios que aportan a su bienestar y salud. Desde el principio, nos hemos enfocado en ofrecer ingredientes naturales de alta calidad, apoyando a productores locales y promoviendo la riqueza agrícola de Paraguay. "Lo natural que alimenta tu felicidad." o como nosotros decimos "Pe heko ypyva omongaruva nde vy\' apavē" es más que un eslogan, es nuestra filosofía.',
+    mision: {
+      title: 'Nuestra misión',
+      itemA: 'Objetivos a corto plazo:',
+      descriptionA: 'Consolidar nuestra presencia en Paraguay, optimizar los procesos logísticos y ampliar nuestra cartera de productos para satisfacer las demandas de fraccionadores y fabricantes.',
+      itemB: 'Objetivos a mediano plazo:',
+      descriptionB: 'Diversificar nuestros productos (garbanzo, porotos, anís), establecer alianzas estratégicas con productores locales y certificar la calidad bajo estándares internacionales.',
+      itemC: 'Objetivos a largo plazo:',
+      descriptionC: 'Expandirnos a mercados internacionales, llevando productos paraguayos al mundo y posicionando a Lamur como un embajador de la calidad nacional.',
+    },
+    values: {
+      title: 'Nuestros Valores',
+      subtitle: 'En Namur, nuestros valores son la base de cada acción y decisión. Creemos que el compromiso, la confianza y la tradición son esenciales para ofrecer productos de calidad que respeten a la naturaleza y a las personas. Nuestra pasión por lo natural nos impulsa a garantizar la seguridad alimentaria en cada etapa del proceso.',
+      item1 : '',
+      subtitleI1: '',
+      item2: '',
+      subtitleI2: '',
+      item3: '',
+      subtitleI3: '',
+    },
+    achievements: {
+      title: '',
+      subtitle: '',
+      item1 : '',
+      subtitleI1: '',
+      item2: '',
+      subtitleI2: '',
+      item3: '',
+      subtitleI3: '',
+    }
+  },
+  products: {
+    title: '',
+    subtitle: '',
+    grillaProd: {
+      title: '',
+      subtitle: '',
+
+      oregano: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      lino: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      pasaDeUva: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      chia: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      sesamo: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      coriandro: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      albahaca: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      romero: {
+        title: 'boooca',
+        description: 'boca',
+      },
+      proximamente: {
+        title: 'boooca',
+        description: 'boca',
+      },
+    }
+  },
+  contact: {
+    title: '',
+    subtitle: '',
+    description: '',
+    form: {
+      name: '',
+      email: '',
+      message: '',
+      consent: '',
+      button: '',
+      message2: ''
+    },
+    visit: '',
   },
 };
